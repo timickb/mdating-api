d249762ad1a133f7a88c69c9986d81c3e7f77088
timickb <timikm2002@gmail.com>
2021-02-19 17:23:46 +0300

    Flask + SwaggerUI

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, make_response, send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
+import os
+
+app = Flask(__name__)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/api.yml'
+swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': 'test'})
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route('/')
+def index():
+    return 'mdating server'
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
