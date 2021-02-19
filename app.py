b45acde31759c56c48ea0dd7c0b720f6583208be
timickb <timikm2002@gmail.com>
2021-02-19 17:32:19 +0300

    /swagger -> /docs

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import os
 
 app = Flask(__name__)
 
-SWAGGER_URL = '/swagger'
+SWAGGER_URL = '/docs'
 API_URL = '/static/api.yml'
-swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': 'test'})
+swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': 'API docs'})
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 @app.route('/')
@@ -18,4 +18,4 @@ def send_static(path):
     return send_from_directory('static', path)
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000)
+    app.run("0.0.0.0", port=5000, debug=True)
